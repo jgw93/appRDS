0c666b70621376fd53165676b8412fdd507c332c
jgw1993 <kmmh99@naver.com>
2024-06-04 16:31:40 +0900

    first commit

---
+++ src/main/java/org/zerock/apprds/config/CustomSecurityConfig.java
@@ -0,0 +1,69 @@
+package org.zerock.apprds.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+
+import java.util.Arrays;
+
+@Configuration
+@Log4j2
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class CustomSecurityConfig {
+
+  @Bean
+  public PasswordEncoder passwordEncoder(){
+    // 암호화 설정
+    return new BCryptPasswordEncoder();
+  }
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer(){
+    log.info("---------------web configure-----------------");
+    // 정적 파일 요청 무시하는 설정
+    return (web) -> web.ignoring()
+        .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain (final HttpSecurity http) throws Exception {
+    log.info("---------------configure-----------------------");
+
+    return http.build();
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
